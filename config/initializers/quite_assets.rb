d8f1dc7ef5fc754ff81e334871a735c12f57eb86
basicdays <paul@basicdays.com>
2013-01-21 13:18:58 -0600

    Remove coffeescript, asset logging

---
+++ config/initializers/quite_assets.rb
@@ -0,0 +1,14 @@
+if Rails.env.development?
+  log_name = ((ENV['OS'] =~ /windows/i).nil?) ? '/dev/null': 'NUL'
+  Rails.application.assets.logger = Logger.new(log_name)
+  Rails::Rack::Logger.class_eval do
+    def call_with_quiet_assets(env)
+      previous_level = Rails.logger.level
+      Rails.logger.level = Logger::ERROR if env['PATH_INFO'] =~ %r{^/assets/}
+      call_without_quiet_assets(env)
+    ensure
+      Rails.logger.level = previous_level
+    end
+    alias_method_chain :call, :quiet_assets
+  end
+end
